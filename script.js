d29837a25444ac9702e958d0a4d797745e9f675f
unknown <siirajulisllam@gmail.com>
2023-10-22 23:52:00 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+function clr(){
+    document.getElementById("scrn").value = "";
+}
+
+
+function display(value){
+    if(value == 'Backspace'){
+        
+        let str = document.getElementById("scrn").value;
+        str = str.toString();
+        str = str.slice(0, -1);
+        console.log(str);
+        document.getElementById("scrn").value = str;
+    }
+    else{
+        document.getElementById("scrn").value += value;
+    }
+}
+
+function calculate(){
+    let p = document.getElementById("scrn").value;
+    p = eval(p);
+    document.getElementById("scrn").value = p;
+}
+
+
+window.addEventListener('keydown',(e)=>{
+    console.log(e.key);
+    if((e.key>=0 && e.key<=9) || e.key == 'Backspace' || e.key =='+' || e.key =='-' || e.key =='*' || e.key =='/' || e.key =='.' ){
+        display(e.key);
+    }
+    else if(e.key == '='){
+        calculate();
+    }
+})
